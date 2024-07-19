903c739cdc0fd8c19f0de054ec7046e8419bb9bd
Robert M <99871728+Robert010600@users.noreply.github.com>
2024-07-18 18:58:09 -0700

    Made small minor changes

--- Section1/src/Exercise2_LiquidContainersOOP/Main.java
+++ Section1/src/Exercise2_LiquidContainersOOP/Main.java
@@ -2,6 +2,6 @@ package Exercise2_LiquidContainersOOP;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("I am in Exercise2");
+        System.out.println("I am in Exercise2 and testing git");
     }
 }
